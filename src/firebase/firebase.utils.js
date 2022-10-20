ef7bbcf2464161b2d7f3dff8997bd28ac76088cb
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-10-20 15:53:02 +0200

    Firebase config edit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,25 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const config = {
-  apiKey: "AIzaSyDyctImpR8hIG3ZcIIvq36Ai5J_h1fiels",
-  authDomain: "king-store-8888.firebaseapp.com",
-  projectId: "king-store-8888",
-  storageBucket: "king-store-8888.appspot.com",
-  messagingSenderId: "666237931132",
-  appId: "1:666237931132:web:5cce93431cf05ccac7c3f9",
-  measurementId: "G-RN3DQ88620"
+  apiKey: "AIzaSyCX72gfHsPjTfVg0ckJ4TtdImjzDRwArE8",
+  authDomain: "crwn-db-8888.firebaseapp.com",
+  projectId: "crwn-db-8888",
+  storageBucket: "crwn-db-8888.appspot.com",
+  messagingSenderId: "343432377299",
+  appId: "1:343432377299:web:4822a9db9d4c5ce2b97526",
+  measurementId: "G-K5WVGKXPBH",
 };
-
+// const config = {
+//   apiKey: "AIzaSyDyctImpR8hIG3ZcIIvq36Ai5J_h1fiels",
+//   authDomain: "king-store-8888.firebaseapp.com",
+//   projectId: "king-store-8888",
+//   storageBucket: "king-store-8888.appspot.com",
+//   messagingSenderId: "666237931132",
+//   appId: "1:666237931132:web:5cce93431cf05ccac7c3f9",
+//   measurementId: "G-RN3DQ88620"
+// };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -21,93 +28,86 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
-    try { 
+    try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
+        ...additionalData,
+      });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
   return userRef;
-}
-
+};
 
 firebase.initializeApp(config);
 
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore.collection("carts").where("userId", "==", userId);
+  const snapShot = await cartsRef.get();
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection("carts").doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
 
-
-
-export const getUserCartRef = async userId => {
-    const cartsRef = firestore.collection('carts').where('userId', '==', userId);
-    const snapShot = await cartsRef.get();
-    if (snapShot.empty) {
-      const cartDocRef = firestore.collection('carts').doc();
-      await cartDocRef.set({ userId, cartItems: [] });
-      return cartDocRef;
-    } else {
-      return snapShot.docs[0].ref;
-    }
-  };
-
-
-
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
 
-  const batch = firestore.batch()
-  objectsToAdd.forEach(obj => {
-    const newDocRef = collectionRef.doc()
-    batch.set(newDocRef, obj)
-    console.log(newDocRef)
-  })
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+    console.log(newDocRef);
+  });
 
-  return await batch.commit()
-}
+  return await batch.commit();
+};
 
 export const convertCollectionsSnapshotToMap = (collections) => {
-  const transformedCollection = collections.docs.map( doc => {
-    const { title, items } = doc.data()
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
-      items
-    }
-  })
+      items,
+    };
+  });
 
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
-  }, {})
-}
+  }, {});
+};
 
-
-export const getCurrentUser =() => {
-  return new Promise((resolve, reject) =>{
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      unsubscribe()
-      resolve(userAuth)
-    }, reject)
-    })
-  }
-
-
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: 'select_account' });
+googleProvider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
-
 
 export default firebase;
